601678025339cd61ccea68267a918925c02af3cf
awandaaa <2857255346@qq.com>
2025-07-28 21:06:52 +0800

    引入依赖ollama

---
+++ src/main/java/com/ght666/aitools/AiToolsApplication.java
@@ -0,0 +1,13 @@
+package com.ght666.aitools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ght666/aitools/AiToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ght666.aitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
