3ff8cf2bd1840defccce30126f0177136d23f6e8
1012ght <haotian.gao@xsuperzone.com>
2025-07-29 10:22:35 +0800

    openai 依赖及配置

--- src/main/java/com/ght666/aiTools/config/CommonConfiguration.java
+++ src/main/java/com/ght666/aiTools/config/CommonConfiguration.java
@@ -1,7 +1,10 @@
 package com.ght666.aiTools.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,15 +12,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CommonConfiguration {
 
+    //注册ChatMemory对象
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
     // 注意参数中的model就是使用的模型，这里用了Ollama，也可以选择OpenAIChatModel
     @Bean
-    public ChatClient chatClient(OllamaChatModel model) {
+    public ChatClient chatClient(OllamaChatModel model, ChatMemory chatMemory) {
         // 创建ChatClient工厂
         // 添加默认的Advisor,记录日志
+        //
         return ChatClient.builder(model)
                 .defaultSystem("你是一个热心、可爱的智能助手，你的名字叫小团团，请以小团团的身份和语气回答问题。")
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
                 .build(); // 构建ChatClient实例
-
     }
 }
--- src/main/java/com/ght666/aiTools/controller/ChatController.java
+++ src/main/java/com/ght666/aiTools/controller/ChatController.java
@@ -1,18 +1,28 @@
 package com.ght666.aiTools.controller;
 
+import com.ght666.aiTools.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
+@CrossOrigin("*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/ai")
 public class ChatController {
 
     private final ChatClient chatClient;
+
+    private final ChatMemory chatMemory;
+
+    private final ChatHistoryRepository chatHistoryRepository;
     // chat
     /*@RequestMapping("/chat")
     public String chat(@RequestParam(defaultValue = "讲个笑话") String prompt) {
@@ -21,12 +31,14 @@ public class ChatController {
                 .call() // 同步请求，会等待AI全部输出完才返回结果
                 .content(); //返回响应内容
     }*/
-    //改为流式调用
+    // 改为流式调用
     // 注意看返回值，是Flux<String>，也就是流式结果，另外需要设定响应类型和编码，不然前端会乱码
     @RequestMapping(value = "/chat", produces = "text/html;charset=UTF-8")
-    public Flux<String> chat(@RequestParam(defaultValue = "讲个笑话") String prompt) {
+    public Flux<String> chat(@RequestParam(defaultValue = "讲个笑话") String prompt,String chatId) {
+        chatHistoryRepository.save("chat", chatId);
         return chatClient
                 .prompt(prompt)
+                .advisors(a->a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
                 .stream() // 流式调用
                 .content();
     }
