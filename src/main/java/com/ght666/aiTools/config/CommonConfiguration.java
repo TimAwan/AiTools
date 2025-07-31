20c885f07ebb9135e6fc59ada71f5fa56b4a1e4c
1012ght <haotian.gao@xsuperzone.com>
2025-07-31 11:09:59 +0800

    rag初始架构（PDF）

--- src/main/java/com/ght666/aiTools/config/CommonConfiguration.java
+++ src/main/java/com/ght666/aiTools/config/CommonConfiguration.java
@@ -3,11 +3,16 @@ package com.ght666.aiTools.config;
 import com.ght666.aiTools.tools.CourseTools;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import static com.ght666.aiTools.constants.SystemConstants.CUSTOMER_SERVICE_SYSTEM;
@@ -34,6 +39,8 @@ public class CommonConfiguration {
                 .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
                 .build(); // 构建ChatClient实例
     }
+
+    //客服聊天模块的配置
     @Bean
     public ChatClient serviceChatClient(
             OpenAiChatModel model,
@@ -45,6 +52,34 @@ public class CommonConfiguration {
                         new MessageChatMemoryAdvisor(chatMemory), // CHAT MEMORY
                         new SimpleLoggerAdvisor())
                 .defaultTools(courseTools)
+                .build();
+    }
+
+    //向量存储
+    @Bean
+    public VectorStore vectorStore(OpenAiEmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
+
+    //PDF
+    @Bean
+    public ChatClient pdfChatClient(
+            OpenAiChatModel model,
+            ChatMemory chatMemory,
+            VectorStore vectorStore) {
+        return ChatClient.builder(model)
+                .defaultSystem("请根据提供的上下文回答问题，不要自己猜测。")
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory), // CHAT MEMORY
+                        new SimpleLoggerAdvisor(),
+                        new QuestionAnswerAdvisor(
+                                vectorStore, // 向量库
+                                SearchRequest.builder() // 向量检索的请求参数
+                                        .similarityThreshold(0.5d) // 相似度阈值
+                                        .topK(2) // 返回的文档片段数量
+                                        .build()
+                        )
+                )
                 .build();
     }
 }
